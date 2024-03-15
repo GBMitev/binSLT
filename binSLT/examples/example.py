767878cceb537c6ba09acfd0386aedfd7f2c6748
GBMitev <68308799+GBMitev@users.noreply.github.com>
2024-03-15 12:40:41 +0000

    Commented example.py

--- binSLT/examples/example.py
+++ binSLT/examples/example.py
@@ -1,22 +1,26 @@
-# %%
+# Import needed packages
 from binslt import binslt as blt
 from binslt.dependencies import *
-# %%
+# initialize full dataset (df) and Duo stable energies (Duo)
 df  = pd.read_csv("path/to/full_data_set")
 Duo = pd.read_csv("path/to/Duo_energies.states",delim_whitespace = True, names = ["NN","E","g","J","tau","e/f","Manifold","v","Lambda","Sigma","Omega"])
-# # %%
+# initialize sigma_range and bin_range
 sigmas = [3,4,5]
 bins   = np.arange(30,400,1)
+# run BinSLT algorithm
 lt = blt.BinSLT(
-  df,
-  sigmas, 
-  bins, 
-  Duo = Duo, 
-  cores = 16,
-  moct = "median",
-  lock = 10,
-  try_without_Duo = False,
-  blt_init = False
+  df, # full dataset
+  sigmas, # sigma_range
+  bins, # bin_range
+  Duo = Duo, #stable Duo energies
+  cores = 16, # number of cores to use in calculation
+  moct = "median", # measure of central tendency for data wranling, see wrangling.cutoff()
+  lock = 10, # allowed energy deviation from Duo stable energy, see wrangling.le_wrt_duo()
+  try_without_Duo = False, # if le_wrt_duo() fails, try using wrangling.filter()
+  blt_init = False # initialise empty BinSLT class instance, used with binslt.read_blt()
   )
+# save solution as *.blt file
+lt.make_sandwich("calculated_predissociation_lifetimes") #.blt file extension added automatically if not present
 
-lt.make_sandwich("calculated_predissociation_lifetimes.blt")
+# load a solution from a *.blt file
+lt = blt.read_blt("calculation_predissociation_lifetimes.blt")
